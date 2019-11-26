f50b50c51aa6aa22ef9b06972143a475f1f5e2ef
saberkane2018 <salwa9aberkane@gmail.com>
2019-11-26 16:15:58 -0500

    critirea for the deletion of the vertex (not finished yet)

--- stl_parser_binary/STL_loader/ClassifyVertex.cpp
+++ stl_parser_binary/STL_loader/ClassifyVertex.cpp
@@ -2,6 +2,9 @@
 #include "ClassifyVertex.h"
 #include <algorithm>
 #include <iostream>
+
+
+/////////////////////////////////////////////////////////////
 bool simpleVertex(stl::Vertex v){
   std::vector <int> index_connected_triang = v.get_connected_triangle();
   stl::Stl_data * data = v.getdata();
@@ -35,4 +38,64 @@ bool simpleVertex(stl::Vertex v){
     }
   }
   return true;
+}
+
+
+//////////////////////////////////////////////////////////////////////////
+
+
+
+std::vector <int> equation_plane(stl::Vertex v1, stl::Vertex v2,stl::Vertex v3)
+{
+
+  float a1= v2.getx() - v1.getx();
+  float b1= v2.gety() - v1.gety();
+  float c1= v2.getz() - v1.getz();
+  float a2= v3.getx() - v1.getx();
+  float b2= v3.gety() - v1.gety();
+  float c2= v3.getz() - v1.getz();
+  float a= b1*c2 - b2*c1;
+  float b= a2*c1 - a1*c2;
+  float c= a1*b2 - b1*a2;
+  float d=( - a*v1.getx() - b*v1.gety() - c*v1.getz());
+  std::vector <int> plane;
+  plane.push_back(a);
+  plane.push_back(b);
+  plane.push_back(c);
+  plane.push_back(d);
+  return plane;
+
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+
+float distance_to_plane(stl::Vertex v1, stl::Vertex v2,stl::Vertex v3,stl::Vertex v)
+{
+  float dist;
+  std::vector<int> plane;
+  plane = equation_plane(v1, v2, v3);
+  float 
+
+
+//// a completer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return dist;
+
 }
--- stl_parser_binary/STL_loader/ClassifyVertex.h
+++ stl_parser_binary/STL_loader/ClassifyVertex.h
@@ -2,8 +2,14 @@
 #include "parse_stl.h"
 #include "tools.h"
 
+
+//classify vertex
 bool simpleVertex(stl::Vertex v);
 bool complexVertex(stl::Vertex v);
 bool boundaryVertex(stl::Vertex v);
 bool interiorEdgeVertex(stl::Vertex v);
 bool cornerVertex(stl::Vertex v);
+
+//criteria for approving the deletion of the vertex or not
+std::vector <int> equation_plane(stl::Vertex v1, stl::Vertex v2,stl::Vertex v3);
+float distance_to_plane(stl::Vertex v1, stl::Vertex v2,stl::Vertex v3,stl::Vertex v);
